6aa27bdd9b0f7eb4d6ec25f85f4b54ccd1aedde7
LiaoLiao <yxiaosong002@gmail.com>
2021-06-29 16:08:10 +0800

    chore: remove unnecessary code (#30)

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,22 +1,3 @@
-import { RedisError, TIMEOUT_EXCEEDED } from '../errors';
-
-/**
- * Executes promise in the given timeout. If the promise does not finish in the given timeout, it will reject.
- *
- * @param ms - The timeout in milliseconds
- * @param promise - The promise that needs to execute
- */
-export const promiseTimeout = (ms: number, promise: Promise<unknown>): Promise<unknown> => {
-    const timeout = new Promise((_, reject) => {
-        const id = setTimeout(() => {
-            clearTimeout(id);
-            reject(new RedisError(TIMEOUT_EXCEEDED(ms)));
-        }, ms);
-    });
-
-    return Promise.race([timeout, promise]);
-};
-
 /**
  * Parses namespace to string.
  *
