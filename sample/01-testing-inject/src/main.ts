5ec42db7beba6c920d8079c96ec390b35d228ea2
LiaoLiao <yxiaosong002@gmail.com>
2021-07-30 20:58:19 +0800

    chore: update .gitignore

--- sample/01-testing-inject/src/main.ts
+++ sample/01-testing-inject/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
     const app = await NestFactory.create(AppModule);
+
+    app.enableShutdownHooks();
+
     await app.listen(3000);
-}
-bootstrap();
+};
+
+void bootstrap();
