a693269d5beaef4ef60c45d559042de6972c802f
LiaoLiao <yxiaosong002@gmail.com>
2021-07-30 19:11:19 +0800

    chore: remove eslint-plugin-eslint-comments
    - update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:eslint-comments/recommended',
         'plugin:jest/recommended',
         'plugin:jest/style',
         'plugin:prettier/recommended'
