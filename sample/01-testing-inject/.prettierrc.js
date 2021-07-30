6129727e693f548258c5c21f48b6255e4afe8121
LiaoLiao <yxiaosong002@gmail.com>
2021-07-30 17:42:16 +0800

    chore: add sample (#67)

---
+++ sample/01-testing-inject/.prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'avoid',
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'strict',
+    vueIndentScriptAndStyle: true,
+    endOfLine: 'auto',
+    embeddedLanguageFormatting: 'off'
+};
