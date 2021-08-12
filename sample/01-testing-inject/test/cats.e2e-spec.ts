35092100ea21fe1ca364274dc43be2f0cee846db
LiaoLiao <yxiaosong002@gmail.com>
2021-08-12 23:15:03 +0800

    chore(sample): use fastify

--- sample/01-testing-inject/test/cats.e2e-spec.ts
+++ sample/01-testing-inject/test/cats.e2e-spec.ts
@@ -1,21 +1,21 @@
-/* eslint-disable jest/expect-expect */
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
+import { FastifyInstance } from 'fastify';
 import { AppModule } from '../src/app.module';
 
 const testCatName = 'Test Cat 3';
 
 describe('Cats', () => {
-    let app: INestApplication;
+    let app: NestFastifyApplication;
 
     beforeAll(async () => {
         const module: TestingModule = await Test.createTestingModule({
             imports: [AppModule]
         }).compile();
 
-        app = module.createNestApplication();
+        app = module.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
         await app.init();
+        await (app.getHttpAdapter().getInstance() as FastifyInstance).ready();
     });
 
     afterAll(async () => {
@@ -24,36 +24,47 @@ describe('Cats', () => {
 
     describe('/cats GET', () => {
         test('should return an array of cats', () => {
-            return request(app.getHttpServer())
-                .get('/cats')
-                .expect(200, [
-                    {
-                        id: 1,
-                        name: 'Test Cat 1',
-                        age: 1,
-                        breed: 'Test Breed 1'
-                    },
-                    {
-                        id: 2,
-                        name: 'Test Cat 2',
-                        age: 2,
-                        breed: 'Test Breed 2'
-                    }
-                ]);
+            return app
+                .inject({
+                    method: 'GET',
+                    url: '/cats'
+                })
+                .then(res => {
+                    expect(res.statusCode).toEqual(200);
+                    expect(JSON.parse(res.payload)).toEqual([
+                        {
+                            id: 1,
+                            name: 'Test Cat 1',
+                            age: 1,
+                            breed: 'Test Breed 1'
+                        },
+                        {
+                            id: 2,
+                            name: 'Test Cat 2',
+                            age: 2,
+                            breed: 'Test Breed 2'
+                        }
+                    ]);
+                });
         });
     });
 
     describe('/cats POST', () => {
         test('should create a cat', () => {
-            return request(app.getHttpServer())
-                .post('/cats')
-                .send({ name: testCatName, age: 3, breed: 'Test Breed 3' })
-                .expect(201)
-                .expect({
-                    id: 3,
-                    name: testCatName,
-                    age: 3,
-                    breed: 'Test Breed 3'
+            return app
+                .inject({
+                    method: 'POST',
+                    url: '/cats',
+                    payload: { name: testCatName, age: 3, breed: 'Test Breed 3' }
+                })
+                .then(res => {
+                    expect(res.statusCode).toEqual(201);
+                    expect(JSON.parse(res.payload)).toEqual({
+                        id: 3,
+                        name: testCatName,
+                        age: 3,
+                        breed: 'Test Breed 3'
+                    });
                 });
         });
     });
