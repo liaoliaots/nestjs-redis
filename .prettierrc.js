fb3850ac8e8a627a8e632ac6b2ac4b03efbdd787
LiaoLiao <yxiaosong002@gmail.com>
2021-09-11 22:28:05 +0800

    chore: update dependencies

--- .prettierrc.js
+++ .prettierrc.js
@@ -8,7 +8,7 @@ module.exports = {
     jsxSingleQuote: false,
     trailingComma: 'none',
     bracketSpacing: true,
-    jsxBracketSameLine: false,
+    bracketSameLine: false,
     arrowParens: 'avoid',
     proseWrap: 'preserve',
     htmlWhitespaceSensitivity: 'strict',
