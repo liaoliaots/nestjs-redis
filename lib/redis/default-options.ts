274f58f2cbecca2b9946a26d136d384f8e0f9e39
LiaoLiao <yxiaosong002@gmail.com>
2021-10-09 17:33:40 +0800

    fix(redis): default config to object

--- lib/redis/default-options.ts
+++ lib/redis/default-options.ts
@@ -3,6 +3,6 @@ import { RedisModuleOptions } from './interfaces';
 export const defaultRedisModuleOptions: RedisModuleOptions = {
     closeClient: false,
     readyLog: false,
-    config: undefined,
+    config: { host: 'localhost', port: 6379 },
     commonOptions: undefined
 };
