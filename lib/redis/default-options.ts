6754319d625a9392d64604267412f8d19457651c
LiaoLiao <yxiaosong002@gmail.com>
2021-10-16 00:04:05 +0800

    refactor: default redis default config option to undefined

--- lib/redis/default-options.ts
+++ lib/redis/default-options.ts
@@ -3,7 +3,6 @@ import { RedisModuleOptions } from './interfaces';
 export const defaultRedisModuleOptions: RedisModuleOptions = {
     closeClient: false,
     readyLog: false,
-    // ! https://github.com/luin/ioredis/blob/master/lib/redis/RedisOptions.ts#L35
-    config: { host: 'localhost', port: 6379 },
+    config: undefined,
     commonOptions: undefined
 };
