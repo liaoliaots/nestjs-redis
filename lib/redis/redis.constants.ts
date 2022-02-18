3849b488b3076ead01b9269158ccbc24eb3bacc0
LiaoLiao <yxiaosong002@gmail.com>
2022-02-19 03:01:19 +0800

    feat!: change default namespace to 'default'

--- lib/redis/redis.constants.ts
+++ lib/redis/redis.constants.ts
@@ -4,7 +4,7 @@ export const REDIS_INTERNAL_OPTIONS = Symbol();
 
 export const REDIS_CLIENTS = Symbol();
 
-export const DEFAULT_REDIS_NAMESPACE = Symbol('default');
+export const DEFAULT_REDIS_NAMESPACE = 'default';
 
 export const REDIS_MODULE_ID = 'RedisModule';
 
