8e4eab6b7f028929172c3367911fa1cf29a25336
LiaoLiao <yxiaosong002@gmail.com>
2022-02-23 01:35:54 +0800

    chore(sample): update

--- sample/01-testing-inject/src/app.module.ts
+++ sample/01-testing-inject/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-    imports: [CatsModule]
+    imports: [
+        RedisModule.forRoot({
+            readyLog: true,
+            config: {
+                host: 'localhost',
+                port: 6380,
+                password: 'bitnami'
+            }
+        }),
+        CatsModule
+    ]
 })
 export class AppModule {}
--- sample/01-testing-inject/src/cats/cats.module.ts
+++ sample/01-testing-inject/src/cats/cats.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
-import { SharedCluster } from '../shared-cluster.module';
 
 @Module({
-    imports: [SharedCluster],
     providers: [CatsService],
     controllers: [CatsController]
 })
