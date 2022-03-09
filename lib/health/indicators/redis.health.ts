58a5c60e18b37f97340400829893ab920d9ed595
LiaoLiao <yxiaosong002@gmail.com>
2022-03-09 23:13:04 +0800

    refactor: update health checks
    - update code comments
    - update messages
    - refactor checkHealth
    - update version

--- lib/health/indicators/redis.health.ts
+++ lib/health/indicators/redis.health.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { HealthIndicator, HealthIndicatorResult, HealthCheckError } from '@nestjs/terminus';
-import { RedisError } from 'redis-errors';
 import {
     FAILED_CLUSTER_STATE,
     CANNOT_BE_READ,
@@ -8,13 +7,13 @@ import {
     NOT_RESPONSIVE,
     ABNORMALLY_MEMORY_USAGE
 } from '@/messages';
-import { isError, promiseTimeout, removeLineBreaks, parseUsedMemory } from '@/utils';
+import { isError, promiseTimeout, removeLineBreaks, parseUsedMemory, isNullish } from '@/utils';
 import { RedisCheckSettings } from './redis-check-settings.interface';
 
 @Injectable()
 export class RedisHealthIndicator extends HealthIndicator {
     /**
-     * Checks a redis/cluster connection.
+     * Checks a redis/cluster client.
      *
      * @param key - The key which will be used for the result object
      * @param options - The extra options for check
@@ -23,8 +22,8 @@ export class RedisHealthIndicator extends HealthIndicator {
      * ```
      * import IORedis from 'ioredis';
      *
-     * const client = new IORedis({ host: 'localhost', port: 6379 });
-     * indicator.checkHealth('redis', { client });
+     * const client = new IORedis({ host: 'localhost', port: 6380 });
+     * indicator.checkHealth('redis', { type: 'redis', client });
      * ```
      *
      * @example
@@ -32,7 +31,7 @@ export class RedisHealthIndicator extends HealthIndicator {
      * import IORedis from 'ioredis';
      *
      * const client = new IORedis.Cluster([{ host: 'localhost', port: 16380 }]);
-     * indicator.checkHealth('cluster', { client });
+     * indicator.checkHealth('cluster', { type: 'cluster', client });
      * ```
      */
     async checkHealth(key: string, options: RedisCheckSettings): Promise<HealthIndicatorResult> {
@@ -40,16 +39,16 @@ export class RedisHealthIndicator extends HealthIndicator {
         let isHealthy = false;
 
         try {
-            if (!options.client) throw new RedisError(CLIENT_NOT_FOUND_FOR_HEALTH);
+            if (!options.client) throw new Error(CLIENT_NOT_FOUND_FOR_HEALTH);
 
             if (type === 'redis') {
                 const pong = await promiseTimeout(options.timeout ?? 1000, options.client.ping());
-                if (pong !== 'PONG') throw new Error(NOT_RESPONSIVE(key));
+                if (pong !== 'PONG') throw new Error(NOT_RESPONSIVE);
 
-                if (options.memoryThreshold) {
+                if (!isNullish(options.memoryThreshold)) {
                     const info = await options.client.info('memory');
                     if (parseUsedMemory(removeLineBreaks(info)) > options.memoryThreshold) {
-                        throw new Error(ABNORMALLY_MEMORY_USAGE(key));
+                        throw new Error(ABNORMALLY_MEMORY_USAGE);
                     }
                 }
             } else {
@@ -57,9 +56,9 @@ export class RedisHealthIndicator extends HealthIndicator {
                 const clusterInfo = await options.client.cluster('info');
 
                 if (clusterInfo && typeof clusterInfo === 'string') {
-                    if (!clusterInfo.includes('cluster_state:ok')) throw new RedisError(FAILED_CLUSTER_STATE);
+                    if (!clusterInfo.includes('cluster_state:ok')) throw new Error(FAILED_CLUSTER_STATE);
                 } else {
-                    throw new RedisError(CANNOT_BE_READ);
+                    throw new Error(CANNOT_BE_READ);
                 }
             }
 
