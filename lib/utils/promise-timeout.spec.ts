dacab09a1c6ca55cd8cf6c11fcc2df6627f85fc3
LiaoLiao <yxiaosong002@gmail.com>
2022-03-14 22:45:31 +0800

    test: use fake timer instead of real timer

--- lib/utils/promise-timeout.spec.ts
+++ lib/utils/promise-timeout.spec.ts
@@ -2,22 +2,24 @@ import { promiseTimeout } from './promise-timeout';
 import { OPERATIONS_TIMEOUT } from '@/messages';
 
 describe('promiseTimeout', () => {
-    let timer: NodeJS.Timeout;
-    const getPromise = (ms: number) => {
-        return new Promise<string>(resolve => {
-            timer = setTimeout(() => resolve('response'), ms);
-        });
-    };
-
-    afterEach(() => {
-        clearTimeout(timer);
+    beforeEach(() => {
+        jest.useFakeTimers();
     });
 
+    const waitPromise = (ms: number) =>
+        new Promise<string>(resolve => {
+            setTimeout(() => resolve('response'), ms);
+        });
+
     test('should work correctly', async () => {
-        await expect(promiseTimeout(20, getPromise(10))).resolves.toBe('response');
+        const promise = promiseTimeout(2000, waitPromise(1000));
+        jest.runAllTimers();
+        await expect(promise).resolves.toBe('response');
     });
 
     test('should throw an error', async () => {
-        await expect(promiseTimeout(10, getPromise(20))).rejects.toThrow(OPERATIONS_TIMEOUT(10));
+        const promise = promiseTimeout(2000, waitPromise(3000));
+        jest.runAllTimers();
+        await expect(promise).rejects.toThrow(OPERATIONS_TIMEOUT(2000));
     });
 });
