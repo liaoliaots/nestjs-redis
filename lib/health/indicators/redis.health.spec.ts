a10ae10acf2a42d6385103a970e26b6dd2ce254b
LiaoLiao <yxiaosong002@gmail.com>
2022-03-14 22:50:28 +0800

    test: use fake timer instead of real timer (#207)

--- lib/health/indicators/redis.health.spec.ts
+++ lib/health/indicators/redis.health.spec.ts
@@ -6,7 +6,9 @@ import {
     CANNOT_BE_READ,
     MISSING_CLIENT,
     NOT_RESPONSIVE,
-    ABNORMALLY_MEMORY_USAGE
+    ABNORMALLY_MEMORY_USAGE,
+    MISSING_TYPE,
+    OPERATIONS_TIMEOUT
 } from '@/messages';
 
 describe('RedisHealthIndicator', () => {
@@ -22,9 +24,14 @@ describe('RedisHealthIndicator', () => {
         indicator = module.get<RedisHealthIndicator>(RedisHealthIndicator);
     });
 
-    test('should throw an error if client is not defined', async () => {
-        const client = null as unknown as Redis;
+    test('should throw an error if the Argument `client` is not defined', async () => {
+        const client = undefined as unknown as Redis;
         await expect(indicator.checkHealth('', { type: 'redis', client })).rejects.toThrow(MISSING_CLIENT);
+    });
+
+    test('should throw an error if the Argument `type` is not defined', async () => {
+        const type = undefined as unknown as 'redis';
+        await expect(indicator.checkHealth('', { type, client: redisClient })).rejects.toThrow(MISSING_TYPE);
     });
 
     describe('redis', () => {
@@ -44,28 +51,32 @@ describe('RedisHealthIndicator', () => {
             });
         });
 
-        test('should throw an error if the result of ping is not PONG', async () => {
-            jest.spyOn(redisClient, 'ping').mockResolvedValue('');
-
-            await expect(indicator.checkHealth('redis', { type: 'redis', client: redisClient })).rejects.toThrow(
-                NOT_RESPONSIVE
-            );
-        });
-
-        test('the status should be down if error is not an instance of Error', async () => {
-            jest.spyOn(redisClient, 'ping').mockRejectedValue(undefined);
-
-            await expect(indicator.checkHealth('redis', { type: 'redis', client: redisClient })).resolves.toEqual({
-                redis: { status: 'down' }
-            });
-        });
-
         test('should throw an error if ping is rejected', async () => {
             const message = 'a redis error';
             jest.spyOn(redisClient, 'ping').mockRejectedValue(new Error(message));
 
-            await expect(indicator.checkHealth('redis', { type: 'redis', client: redisClient })).rejects.toThrow(
-                message
+            await expect(indicator.checkHealth('', { type: 'redis', client: redisClient })).rejects.toThrow(message);
+        });
+
+        test('should throw an error if ping timed out', async () => {
+            jest.useFakeTimers();
+
+            const waitPromise = (ms: number) =>
+                new Promise<string>(resolve => {
+                    setTimeout(() => resolve('PONG'), ms);
+                });
+
+            jest.spyOn(redisClient, 'ping').mockImplementation(() => waitPromise(2000));
+            const promise = indicator.checkHealth('', { type: 'redis', client: redisClient });
+            jest.runAllTimers();
+            await expect(promise).rejects.toThrow(OPERATIONS_TIMEOUT(1000));
+        });
+
+        test('should throw an error if the result of ping is not PONG', async () => {
+            jest.spyOn(redisClient, 'ping').mockResolvedValue('');
+
+            await expect(indicator.checkHealth('', { type: 'redis', client: redisClient })).rejects.toThrow(
+                NOT_RESPONSIVE
             );
         });
 
@@ -80,6 +91,14 @@ describe('RedisHealthIndicator', () => {
                     memoryThreshold: 1000 * 100
                 })
             ).rejects.toThrow(ABNORMALLY_MEMORY_USAGE);
+        });
+
+        test('the status should be down if the error is not an instance of Error', async () => {
+            jest.spyOn(redisClient, 'ping').mockRejectedValue(undefined);
+
+            await expect(indicator.checkHealth('redis', { type: 'redis', client: redisClient })).resolves.toEqual({
+                redis: { status: 'down' }
+            });
         });
     });
 
@@ -98,7 +117,7 @@ describe('RedisHealthIndicator', () => {
             const message = 'a redis error';
             jest.spyOn(clusterClient, 'cluster').mockRejectedValue(new Error(message));
 
-            await expect(indicator.checkHealth('cluster', { type: 'cluster', client: clusterClient })).rejects.toThrow(
+            await expect(indicator.checkHealth('', { type: 'cluster', client: clusterClient })).rejects.toThrow(
                 message
             );
         });
@@ -106,7 +125,7 @@ describe('RedisHealthIndicator', () => {
         test('should throw an error if cluster-info is null', async () => {
             jest.spyOn(clusterClient, 'cluster').mockResolvedValue(null);
 
-            await expect(indicator.checkHealth('cluster', { type: 'cluster', client: clusterClient })).rejects.toThrow(
+            await expect(indicator.checkHealth('', { type: 'cluster', client: clusterClient })).rejects.toThrow(
                 CANNOT_BE_READ
             );
         });
@@ -114,7 +133,7 @@ describe('RedisHealthIndicator', () => {
         test('should throw an error if cluster-info does not contain "cluster_state:ok"', async () => {
             jest.spyOn(clusterClient, 'cluster').mockResolvedValue('cluster_state:fail');
 
-            await expect(indicator.checkHealth('cluster', { type: 'cluster', client: clusterClient })).rejects.toThrow(
+            await expect(indicator.checkHealth('', { type: 'cluster', client: clusterClient })).rejects.toThrow(
                 FAILED_CLUSTER_STATE
             );
         });
