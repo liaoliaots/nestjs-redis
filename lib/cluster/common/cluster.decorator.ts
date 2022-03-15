91184ead65fb968d6d34c7d9cc586ac1fa9563de
LiaoLiao <yxiaosong002@gmail.com>
2022-03-15 23:26:54 +0800

    refactor: simplify decorator

--- lib/cluster/common/cluster.decorator.ts
+++ lib/cluster/common/cluster.decorator.ts
@@ -1,24 +1,19 @@
 import { Inject } from '@nestjs/common';
 import { DEFAULT_CLUSTER_NAMESPACE, CLUSTER_MODULE_ID } from '../cluster.constants';
 import { ClientNamespace } from '@/interfaces';
-import { isString, isSymbol } from '@/utils';
+import { isSymbol } from '@/utils';
 
 export const namespaces = new Map<ClientNamespace, ClientNamespace>();
 
 /**
- * This decorator is used to mark a specific class property as a cluster client.
+ * This decorator is used to mark a specific constructor parameter as a cluster client.
  *
  * @param namespace - Client name
  */
-export const InjectCluster = (namespace: ClientNamespace = DEFAULT_CLUSTER_NAMESPACE): ReturnType<typeof Inject> => {
-    if (isString(namespace)) {
-        const token = getClusterToken(namespace);
-        namespaces.set(namespace, token);
-        return Inject(token);
-    }
-
-    namespaces.set(namespace, namespace);
-    return Inject(namespace);
+export const InjectCluster = (namespace: ClientNamespace = DEFAULT_CLUSTER_NAMESPACE): ParameterDecorator => {
+    const token = getClusterToken(namespace);
+    namespaces.set(namespace, token);
+    return Inject(token);
 };
 
 /**
