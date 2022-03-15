71a8a42884c7e3513a654aa43fd330ea338c90e2
LiaoLiao <yxiaosong002@gmail.com>
2022-03-15 23:30:34 +0800

    refactor: simplify decorator (#210)

--- lib/redis/common/redis.decorator.ts
+++ lib/redis/common/redis.decorator.ts
@@ -1,24 +1,19 @@
 import { Inject } from '@nestjs/common';
 import { DEFAULT_REDIS_NAMESPACE, REDIS_MODULE_ID } from '../redis.constants';
 import { ClientNamespace } from '@/interfaces';
-import { isString, isSymbol } from '@/utils';
+import { isSymbol } from '@/utils';
 
 export const namespaces = new Map<ClientNamespace, ClientNamespace>();
 
 /**
- * This decorator is used to mark a specific class property as a redis client.
+ * This decorator is used to mark a specific constructor parameter as a redis client.
  *
  * @param namespace - Client name
  */
-export const InjectRedis = (namespace: ClientNamespace = DEFAULT_REDIS_NAMESPACE): ReturnType<typeof Inject> => {
-    if (isString(namespace)) {
-        const token = getRedisToken(namespace);
-        namespaces.set(namespace, token);
-        return Inject(token);
-    }
-
-    namespaces.set(namespace, namespace);
-    return Inject(namespace);
+export const InjectRedis = (namespace: ClientNamespace = DEFAULT_REDIS_NAMESPACE): ParameterDecorator => {
+    const token = getRedisToken(namespace);
+    namespaces.set(namespace, token);
+    return Inject(token);
 };
 
 /**
