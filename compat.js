78080d4d601b0870a55e10b99dd15d22191a8e01
LiaoLiao <yxiaosong002@gmail.com>
2022-04-14 16:54:10 +0800

    chore(deps): remove core-js-compat

--- compat.js
+++
@@ -1,9 +0,0 @@
-// https://docs.nestjs.com/first-steps#prerequisites
-// https://node.green/
-
-const { targets } = require('core-js-compat')({
-    targets: 'node 12.22',
-    filter: /^es\./,
-    version: '3.21'
-});
-console.log('targets:', targets);
