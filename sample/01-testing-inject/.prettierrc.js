39e937f179a767370f1d51d9cf89dab8510b36b5
LiaoLiao <yxiaosong002@gmail.com>
2022-05-27 22:33:08 +0800

    chore(sample): update

--- sample/01-testing-inject/.prettierrc.js
+++ sample/01-testing-inject/.prettierrc.js
@@ -1,17 +1,10 @@
 module.exports = {
     printWidth: 120,
     tabWidth: 4,
-    useTabs: false,
-    semi: true,
     singleQuote: true,
-    quoteProps: 'as-needed',
-    jsxSingleQuote: false,
     trailingComma: 'none',
-    bracketSpacing: true,
-    bracketSameLine: false,
     arrowParens: 'avoid',
-    proseWrap: 'preserve',
     htmlWhitespaceSensitivity: 'strict',
     vueIndentScriptAndStyle: true,
-    endOfLine: 'lf'
+    endOfLine: 'auto'
 };
