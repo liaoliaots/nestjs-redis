35ef343c9ec13791e09e108764c58accf920aa67
LiaoLiao <yxiaosong002@gmail.com>
2022-07-12 22:58:23 +0800

    refactor: update

--- packages/nestjs-redis-health/lib/utils/index.spec.ts
+++ packages/nestjs-redis-health/lib/utils/index.spec.ts
@@ -1,7 +1,7 @@
 import * as allExports from '.';
 
-test('there should be 10 exports', () => {
-  expect(Object.keys(allExports)).toHaveLength(10);
+test('there should be 4 exports', () => {
+  expect(Object.keys(allExports)).toHaveLength(4);
 });
 
 test('each of exports should be defined', () => {
