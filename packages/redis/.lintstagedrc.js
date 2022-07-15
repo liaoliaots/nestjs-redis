2528657cc6ac25bfa684073f172e7b2243b00da8
LiaoLiao <yxiaosong002@gmail.com>
2022-07-15 22:25:29 +0800

    chore: update template, add LICENSE, update config (#311)

--- packages/redis/.lintstagedrc.js
+++ packages/redis/.lintstagedrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  '{lib,test}/**/*.ts': ['prettier --write', 'eslint', () => 'tsc -p tsconfig.json --noEmit']
+  '{lib,test}/**/*.ts': ['prettier --write', 'eslint', () => 'tsc -p tsconfig.build.json --noEmit']
 };
