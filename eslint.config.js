f01aa5f60ade1edc19644de905a47fd0dd3db35d
LiaoLiao <yxiaosong002@gmail.com>
2024-09-13 22:50:38 +0800

    chore: update eslint&typescript conf

---
+++ eslint.config.js
@@ -0,0 +1,4 @@
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended);
