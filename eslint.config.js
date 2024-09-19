470f6dc5185e8e4e49ff43012aeb812f3612b41a
LiaoLiao <yxiaosong002@gmail.com>
2024-09-20 03:27:55 +0800

    chore: update typescript & eslint configuration (#541)

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,41 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
+import prettier from 'eslint-plugin-prettier/recommended';
+// @ts-ignore
 import jest from 'eslint-plugin-jest';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default tseslint.config({
-  extends: [
-    eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    {
-      ...jest.configs['flat/recommended']
-    },
-    {
-      ...jest.configs['flat/style']
-    },
-    eslintPluginPrettierRecommended
-  ],
-  languageOptions: {
-    parserOptions: {
-      project: ['./packages/*/tsconfig.json'],
-      tsconfigRootDir: import.meta.dirname
-    },
-    ecmaVersion: 2023,
-    sourceType: 'commonjs',
-    globals: {
-      ...globals.node,
-      ...globals.jest
+export default tseslint.config(
+  {
+    ignores: ['.prettierrc.js', 'eslint.config.js', 'sample', '**/dist', 'packages/*/*.js']
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    files: ['packages/**/test/**', 'packages/**/*.spec.ts'],
+    ...jest.configs['flat/recommended']
+  },
+  {
+    files: ['packages/**/test/**', 'packages/**/*.spec.ts'],
+    ...jest.configs['flat/style']
+  },
+  prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      },
+      parserOptions: {
+        project: ['./packages/*/tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname
+      }
     }
   },
-  ignores: ['.prettierrc.js', 'eslint.config.js', '**/dist/**/*']
-});
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: true
+    }
+  }
+);
