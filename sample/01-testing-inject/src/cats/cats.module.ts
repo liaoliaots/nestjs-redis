7b51929f8af57892ea55c7f872acafa9b97ac1ae
LiaoLiao <yxiaosong002@gmail.com>
2024-09-21 00:48:41 +0800

    chore: remove sample

--- sample/01-testing-inject/src/cats/cats.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CatsService } from './cats.service';
-import { CatsController } from './cats.controller';
-
-@Module({
-  providers: [CatsService],
-  controllers: [CatsController]
-})
-export class CatsModule {}
--- sample/01-testing-inject/src/cats/create-cat.dto.ts
+++
@@ -1,9 +0,0 @@
-export class CreateCatDto {
-  name: string;
-  breed: string;
-
-  constructor(name: string, breed: string) {
-    this.name = name;
-    this.breed = breed;
-  }
-}
